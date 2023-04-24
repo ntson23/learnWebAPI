e73318445c2c41b49140195e6548f91295a3c51c
Nguyễn Tùng Sơn <ntson230801@gmail.com>
2023-04-24 10:23:19 +0700

    change httpPost

---
+++ learnAPI/learnAPI/Controllers/LoaisController.cs
@@ -0,0 +1,99 @@
+﻿using learnAPI.Data;
+using learnAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace learnAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoaisController : ControllerBase
+    {
+        private  readonly MyDbContext _context;
+
+        public LoaisController(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var dsLoai = _context.Loais.ToList();
+            return Ok(dsLoai);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var loai = _context.Loais.SingleOrDefault(lo => 
+            lo.MaLoai == id);
+
+            if(loai != null)
+            {
+                return Ok(loai);
+            }
+            else
+            {
+                return NotFound();
+            }         
+        }
+
+        [HttpPost]
+        public IActionResult CreateNew(LoaiModel model)
+        {
+            try
+            {
+                var loai = new Loai
+                {
+                    TenLoai = model.TenLoai
+                };
+                _context.Add(loai);
+                _context.SaveChanges();
+                return Ok(loai);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateLoaiById(int id, LoaiModel model)
+        {
+            var loai = _context.Loais.SingleOrDefault(lo =>
+            lo.MaLoai == id);
+
+            if (loai != null)
+            {
+                loai.TenLoai = model.TenLoai;
+                _context.SaveChanges();
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteLoaiById(int id)
+        {
+            var loai = _context.Loais.SingleOrDefault(lo =>
+            lo.MaLoai == id);
+
+            if (loai != null)
+            {
+                _context.Remove(loai);
+                _context.SaveChanges();
+                return StatusCode(StatusCodes.Status200OK);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+    }
+}
